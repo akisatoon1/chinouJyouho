d0b2c31380fa5f7210a4088166455f6398cdaf39
akisatoon <akisatoon@gmail.com>
2025-04-20 01:35:33 +0900

    delete storageKey var

--- src/modules/general.ts
+++ src/modules/general.ts
@@ -2,15 +2,12 @@
  * moduleで使う共通の型や値をまとめている
  */
 
-export { TypeText, TypeSelect, StorageKey, input, storageData }
+export { TypeText, TypeSelect, input, storageData }
 
 // 入力の種類を定義する
 // ex) textarea, select, etc
 const TypeText: string = "text";
 const TypeSelect: string = "select";
-
-// local storageのキー
-const StorageKey: string = "test";
 
 // ページ内のinputタグを見つけて, 値を入力するため
 interface input {
@@ -25,5 +22,5 @@ interface input {
 
 // local storage内でのデータ形式
 interface storageData {
-    [StorageKey]: input[]
+    test: input[]
 }
--- src/modules/storage.ts
+++ src/modules/storage.ts
@@ -2,16 +2,16 @@
  * chrome local storageのデータについて取得や保存をする
  */
 
-import { input, storageData, StorageKey } from "./general"
+import { input, storageData } from "./general"
 export { getFromStorage, saveToStorage }
 
 async function getFromStorage(): Promise<input[]> {
-    const result: storageData = await chrome.storage.local.get([StorageKey]);
-    return result[StorageKey];
+    const result: storageData = await chrome.storage.local.get("test");
+    return result.test;
 }
 
 function saveToStorage(data: input[]): void {
-    const setData: storageData = { [StorageKey]: data };
+    const setData: storageData = { test: data };
     chrome.storage.local.set(setData).then(() => {
         alert("ストレージに保存しました");
     });
