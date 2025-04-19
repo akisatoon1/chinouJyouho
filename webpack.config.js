3c668b022fe9571d05671bf59645e0b3853f31fc
akisatoon <akisatoon@gmail.com>
2025-04-19 19:01:31 +0900

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    entry: {
+        'entry': './src/entry.ts',
+        'inserted/get': './src/inserted/get.ts',
+        'inserted/insert': './src/inserted/insert.ts',
+        'inserted/clear': './src/inserted/clear.ts',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './build/js'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts'],
+    },
+    mode: 'production',
+};
